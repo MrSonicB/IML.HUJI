ecc7d98b66d224d558c0cca3f371a962f373fe76
bar.rousso <bar.rousso@mail.huji.ac.il>
2022-06-09 23:28:51 +0300

    Exercise 5 - PCA, Kernels, Regularization and Cross-Validation

--- IMLearn/model_selection/cross_validate.py
+++ IMLearn/model_selection/cross_validate.py
@@ -37,4 +37,49 @@ def cross_validate(estimator: BaseEstimator, X: np.ndarray, y: np.ndarray,
     validation_score: float
         Average validation score over folds
     """
-    raise NotImplementedError()
+    m = X.shape[0]
+    set_size = int(m/cv)
+    remnant = m - set_size * cv
+
+    # Shaffle the indices
+    random_indices = np.random.permutation(m)
+    X = X[random_indices]
+    y = y[random_indices]
+
+    # Perform folding
+    i = 0
+    weight = 0
+    train_score = 0
+    validation_score = 0
+    while i < m:
+        # To keep each group size as even as possible
+        if remnant > 0:
+            remnant -= 1
+            j = 1
+        else:
+            j = 0
+
+        # Split for data and responses to training and test
+        training_data = np.r_[X[:i, :], X[i + set_size + j:, :]]
+        training_true_responses = np.r_[y[:i], y[i + set_size + j:]]
+        test_data = X[i:i + set_size + j, :]
+        test_true_responses = y[i:i + set_size + j]
+
+        # Fit the estimator based on the folded training data
+        estimator.fit(training_data, training_true_responses)
+        # Predict the responses for the training data and test data
+        training_predict_responses = estimator.predict(training_data)
+        test_predict_responses = estimator.predict(test_data)
+
+        # Compute the training and test predictions errors based on the scoring function
+        train_error = scoring(training_true_responses, training_predict_responses)
+        test_error = scoring(test_true_responses, test_predict_responses)
+
+        # Update the averages for train_score and validation_score
+        train_score = (train_score * weight + train_error) / (weight + 1)
+        validation_score = (validation_score * weight + test_error) / (weight + 1)
+
+        i += (set_size + j)
+        weight += 1
+
+    return train_score, validation_score
